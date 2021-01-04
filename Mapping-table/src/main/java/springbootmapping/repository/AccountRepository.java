3b6137aa26355e7006eddbb84eaa50fe05f28a8c
arjun <arjunyadav102030@gmail.com>
2021-01-04 17:26:31 +0530

    initial commit

--- Mapping-table/src/main/java/springbootmapping/repository/AccountRepository.java
+++ Mapping-table/src/main/java/springbootmapping/repository/AccountRepository.java
@@ -10,6 +10,7 @@ import springbootmapping.model.Account;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>{
 
-	List<Account> findByOrderByIdDesc();
+         	List<Account> findByOrderByIdDesc();
+
 
 }
